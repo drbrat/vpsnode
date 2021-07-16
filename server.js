b2fb9c6c722998350066b6961caba38554bd04dd
drbrat <drbrat@protonmail.com>
2021-07-16 04:51:46 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,41 @@
+
+    var express = require('express');
+
+    var app = express();
+
+    var contacts = [
+	{
+	    id: 1,
+	    desc: "desc1"
+	},
+	{
+	    id: 2,
+	    desc: "desc2"
+	}
+    ];
+
+    var about = 'aboutttt...';
+
+    app.get('/', function (req, res) {
+	res.send('Hello API');
+    })
+
+    app.get('/about', function (req, res) {
+	res.send(about);
+    })
+
+    app.get('/contacts', function (req, res) {
+	res.send(contacts);
+    })
+
+    app.get('/contacts/:id', function (req, res) {
+	console.log(req.params);
+	var contact = contacts.find(function(contact) {
+	    return contact.id === Number(req.params.id);
+	})
+	res.send(contact);
+    })
+
+    app.listen(8089, function() {
+	console.log('API app started.');
+    })
